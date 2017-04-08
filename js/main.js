5813afc3676d74503e36d29fc7f26d29a6e590dd
孙昊翔 <673686754@qq.com>
2017-04-08 23:07:08 +0800

    task详情

--- js/main.js
+++ js/main.js
@@ -2,14 +2,22 @@
 	'use strict';
 
 	var $form_add_task = $('.add-task'),
-		$delete_task,
-		task_list = [];
+		$task_delete_trigger,
+		$task_delete,
+		$task_detail_trigger,
+		$task_detail,
+		$task_detail = $('.task-detail'),
+		$task_detail_mask = $('.task-detail-mask'),
+		task_list = [],
+		current_index,
+		$update_form,
+		$task_detail_content,
+		$task_detail_content_input;
 
-
-
-	    init();
+	init();
 
 	$form_add_task.on('submit',on_add_task_form_submit);
+	$task_detail_mask.on('click',hide_task_detail);
 
 	function on_add_task_form_submit(e) {
 		var new_task = {};
@@ -24,11 +32,98 @@
 		if (add_task(new_task)) {
 			$input.val(null);
 		}
-	}	
+	}
+
+	function listen_task_detail() {
+		var index;
+		$('.task-item').on('dblclick',function() {
+			index = $(this).data('index');
+			show_task_detail(index);
+		})
+
+		$task_detail_trigger.on('click',function() {
+			var $this = $(this);
+			var $item = $this.parent().parent();
+			index = $item.data('index');
+			show_task_detail(index);
+		})
+	}
+
+	/*查看task详情*/
+	function show_task_detail(index) {
+		/*生成详情模板*/
+		render_task_detail(index);
+		current_index = index;
+		/*显示详情模板（默认隐藏）*/
+		$task_detail.show();
+		$task_detail_mask.show();
+	}
+
+	/*更新task*/
+	function update_task(index,data) {
+		if(!index === undefined || !task_list[index]) return;
+		task_list[index] = data;
+		refresh_task_list();
+	}
+
+	function hide_task_detail() {
+		$task_detail.hide();
+		$task_detail_mask.hide();
+	}
+
+	function render_task_detail(index) {
+		if(index === undefined || !task_list[index]) return;
+		var item = task_list[index];
+		var tpl = 
+			'<form>' +
+	    	'<div class="content">' +
+	    	item.content +
+	    	'</div>' +
+			'<div class="input-item"><input style="display:none;" type="text" name="content" value="'+
+			(item.content || '') +
+			'"></div>' +
+	    	'<div>' +
+	    	'<div class="desc input-item">' +
+			'<textarea name="desc">' +
+			(item.desc || '') +
+			'</textarea>' +
+	    	'</div>' +
+	    	'</div>' +
+	    	'<div class="remind input-item">' +
+	    	'<input name="remind_date" type="date" value="' +
+			item.remind_date+'">' +
+	    	'</div>' +
+			'<div class="input-item"><button type="submit">更新</button></div>' +
+			'</form>';
+
+		/*清空task详情模板*/
+		$task_detail.html(null);
+		/*添加新模板*/
+		$task_detail.html(tpl);
+		$update_form = $task_detail.find('form');
+		$task_detail_content = $update_form.find('.content');
+		$task_detail_content_input = $update_form.find('[name=content]');
+
+		$task_detail_content.on('dblclick',function() {
+			$task_detail_content_input.show();
+			$task_detail_content.hide();
+		})
+
+		$update_form.on('submit',function(e) {
+			e.preventDefault();
+			var data = {};
+			data.content = $(this).find('[name=content]').val();
+			data.desc = $(this).find('[name=desc]').val();
+			data.remind_date = $(this).find('[name=remind_date]').val();
+			update_task(index,data);
+			hide_task_detail();
+		})
+
+	}
 
 	/*查找并监听所有删除按钮的点击事件*/
 	function listen_task_delete() {
-		$delete_task.on('click',function() {
+		$task_delete_trigger.on('click',function() {
 		    var $this = $(this);
 			/*找到删除按钮所在的task元素*/
 		    var $item = $this.parent().parent();
@@ -72,11 +167,13 @@
 		$task_list.html('');
 		for(var i = 0;i < task_list.length;i++) {
 			var $task = render_task_item(task_list[i],i);
-			$task_list.append($task);
+			$task_list.prepend($task);
 		}
 
-		$delete_task = $('.action.delete');
+		$task_delete_trigger = $('.action.delete');
+		$task_detail_trigger = $('.action.detail');
 		listen_task_delete();
+		listen_task_detail();
     }
     
 	/*渲染单条task模板*/
@@ -88,7 +185,7 @@
             '<span class="task-content">' + data.content + '</span>' +
 			'<span class="fr">' +
             '<span class="action delete"> 删除</span>' +
-            '<span class="action"> 详细</span>' +
+            '<span class="action detail"> 详细</span>' +
 			'</span>' +
             '</div>';
         return $(list_item_tpl);
